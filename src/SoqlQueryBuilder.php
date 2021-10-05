bb9649f3cceb066dc1a773c46b9256f2d5ecda2f
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-10-04 20:18:26 -0700

    First Attempts

--- src/SoqlQueryBuilder.php
+++ src/SoqlQueryBuilder.php
@@ -4,6 +4,8 @@ namespace Salesforce;
 
 class SoqlQueryBuilder{
 
+    public function __construct(){}
+
     public static function getSoqlConditions($values, $fields){
 
         $conditions = array();
@@ -30,4 +32,86 @@ class SoqlQueryBuilder{
 
         return $conditions;
     }
+
+
+    public static function buildConditions($group){
+
+        $builder = new self();
+
+        $sql = "";
+
+        $joinOperator = $group["op"];
+
+        foreach($group["conditions"] as $condition) {
+
+            if(!is_array($condition)) continue;
+
+            if($condition["isGroup"] != True){
+
+                $sql .= $builder->buildCondition($condition, $joinOperator);
+
+            } else {
+
+                foreach($condition["conditions"] as $subcondition){
+
+                    if(!is_array($subcondition)) continue;
+
+                    $joinOperator = $condition["op"];
+
+                    $sql .= $builder->buildCondition($subcondition, $joinOperator);
+
+                }
+            }
+        }
+
+        var_dump($sql);exit;
+
+    }
+
+
+    public function buildCondition($item, $operator){
+
+        return " " . $item["fieldname"] . " " . $item["op"] . " '" . $item["value"] . "' " .  $operator;
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function buildCondition($op, $opnd1, $opnd2 = null){
+
+        $sql = "";
+
+        $args = func_get_args();
+
+
+        if($op == "AND" || $op == "OR"){
+
+            array_shift($args);
+
+            return implode(" $op ", array_map(function($item){
+
+                return buildCondition($item["op"], $item["name"], $item["value"]);
+
+            }, $args));
+
+        } else {
+
+            return "$opnd1 $op $opnd2";
+        }
+    }
