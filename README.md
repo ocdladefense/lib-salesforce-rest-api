dd97fbdb66f36680ae55450495d1cabf2c0ec42c
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-10-05 15:31:02 -0700

    Add a compile function.

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-"lib-salesforce-rest-api" repository
+# "lib-salesforce-rest-api" repository
+
+
+
+
+## SoqlQueryBuilder Class
+
+#### Example: Array structure to be passed to "buildConditions".
+
+```
+
+        $conditionGroup = array(
+            "op" => "AND",
+            "conditions" => array(
+                array(
+                    "fieldname"  => "Ocdla_Current_Member_Flag__c",
+                    "value"      => True,
+                    "op"         => "=",
+                    "syntax"     => "%s"
+                ),
+                array(
+                    "fieldname"  => "FirstName",
+                    "value"      => $_POST["FirstName"],
+                    "op"         => "LIKE",
+                    "syntax"     => "'%%%s%%'"
+                ),
+                array(
+                    "fieldname"  => "LastName",
+                    "value"      => $_POST["LastName"],
+                    "op"         => "LIKE",
+                    "syntax"     => "'%%%s%%'"
+                ),
+                array(
+                    "fieldname"  => "Ocdla_Organization__c",
+                    "value"      => $_POST["Ocdla_Organization__c"],
+                    "op"         => "LIKE",
+                    "syntax"     => "'%%%s%%'"
+                ),
+                array(
+                    "fieldname"  => "MailingCity",
+                    "value"      => $_POST["MailingCity"],
+                    "op"         => "LIKE",
+                    "syntax"     => "'%%%s%%'"
+                ),
+                array(
+                    "fieldname"  => "Ocdla_Occupation_Field_Type__c",
+                    "value"      => $_POST["Ocdla_Occupation_Field_Type__c"],
+                    "op"         => "LIKE",
+                    "syntax"     => "'%%%s%%'"
+                ),
+                array(
+                    "fieldname"  => "Ocdla_Is_Expert_Witness__c",
+                    "value"      => $_POST["Ocdla_Is_Expert_Witness__c"],
+                    "op"         => "=",
+                    "syntax"     => "%s"
+                )
+                array(
+                    "op" => "OR",
+                    "isGroup" => True,
+                    "conditions" => array(
+                        array(
+                            "fieldname"  => "LastName",
+                            "value"      => "Uehlin",
+                            "op"         => "LIKE",
+                            "syntax"     => "'%%%s%%'",
+                            "isFirst"    => True
+                        ),
+                        array(
+                            "fieldname"  => "LastName",
+                            "value"      => "Johnson",
+                            "op"         => "LIKE",
+                            "syntax"     => "'%%%s%%'"
+                        )
+                    )
+                )
+            )
+        );
+ ```
+
+ #### Example of the array structure for first parameter to be passed to the "getSoqlConditions" function
+ ```
+         $fields = array(
+          "FirstName"                     => "LIKE '%%%s%%'",
+          "LastName"                      => "LIKE '%%%s%%'",
+          "Ocdla_Organization__c"         => "LIKE '%%%s%%'",
+          "MailingCity"                   => "LIKE '%%%s%%'",
+          "Ocdla_Occupation_Field_Type__c"=> "LIKE '%%%s%%'",
+          "Ocdla_Current_Member_Flag__c"  => "= %s",
+          "Ocdla_Is_Expert_Witness__c"    => "= %s"
+        );
+
+```
+
+
--- src/SoqlQueryBuilder.php
+++ src/SoqlQueryBuilder.php
@@ -4,114 +4,171 @@ namespace Salesforce;
 
 class SoqlQueryBuilder{
 
-    public function __construct(){}
+    public $baseQuery;
 
-    public static function getSoqlConditions($values, $fields){
+    public $selectFields;
 
-        $conditions = array();
+    public $conditions;
 
-        $fieldsWithValues = array_filter($values, function($value){
-
-            return ($value !== "" && $value !== null);
-        });
-
-        if(empty($fieldsWithValues)) return null;
+    public $orderBy;
 
 
-        foreach($fieldsWithValues as $field => $value){
 
-            $syntax = $fields[$field];
+    public function __construct($query = null){
 
-            if($syntax == null) continue;
-
-            $value = is_bool($value) ? ($value ? "True" : "False") : $value;
-
-            $formatted = sprintf($syntax, $value);
-            $conditions[] = $field . " " . $formatted;
-        }
-
-        return $conditions;
+        $this->baseQuery = $query;
     }
 
 
-    public static function buildConditions($group){
 
-        $builder = new self();
+    public function buildConditions($conditionGroup){
+
+        // Filter out the conditions where no value is present at the key of "value".  With the exception of grouped conditions.  Reset the keys.
+        $conditions = array_values(array_filter($conditionGroup["conditions"], function($con){
+
+            return $con["isGroup"] == True || ($con["value"] !== null && $con["value"] !== "");
+            
+        }));
+
 
         $sql = "";
+        $joinOperator = $conditionGroup["op"];
 
-        $joinOperator = $group["op"];
-
-        foreach($group["conditions"] as $condition) {
+        foreach($conditions as $condition) {
 
             if(!is_array($condition)) continue;
 
             if($condition["isGroup"] != True){
 
-                $sql .= $builder->buildCondition($condition, $joinOperator);
+                $sql .= $this->buildCondition($condition, $joinOperator);
 
             } else {
 
-                foreach($condition["conditions"] as $subcondition){
+                // Since "isGroup" is set to True, this condition is a group of conditions, so the join operator changes to the op on the parent condition.
+                $joinOperator = $condition["op"];
+
+                // Filter out the conditions with no values, and reset the keys.
+                $subConditions = array_values(array_filter($condition["conditions"], function($con){
+
+                    return $con["isGroup"] == True || ($con["value"] !== null && $con["value"] !== "");
+                    
+                }));
+
+                for($i = 0; $i < count($subConditions); $i++) {
+
+                    if($i == 0) $sql .= "(";
+
+                    $subcondition = $subConditions[$i];
 
                     if(!is_array($subcondition)) continue;
 
-                    $joinOperator = $condition["op"];
-
-                    $sql .= $builder->buildCondition($subcondition, $joinOperator);
-
+                    if(!empty($subcondition["value"])) $sql .= $this->buildCondition($subcondition, $joinOperator);
                 }
+
+                $sql = trim($sql, " $joinOperator");
+                $sql .= ")";
             }
         }
 
-        var_dump($sql);exit;
-
+        $this->conditions = trim($sql, " $joinOperator");
     }
 
 
     public function buildCondition($item, $operator){
 
-        return " " . $item["fieldname"] . " " . $item["op"] . " '" . $item["value"] . "' " .  $operator;
+        $value = $item["value"];
 
+        $value = is_bool($value) ? ($value ? "True" : "False") : $value;
+
+        $formattedValue = sprintf($item["syntax"], $value);
+
+        return $item["fieldname"] . " " . $item["op"] . " $formattedValue $operator ";
+    }
+
+
+    public function setQuery($query) {
+
+        $this->baseQuery = $query;
+    }
+
+    public function setOrderBy($orderBy) {
+
+        $this->orderBy = $orderBy;
+    }
+
+
+    // Just takes the entire condition string right now.
+    public function addCondition($entireConditionString) {
+
+        $this->conditions .= $entireConditionString;
+    }
+
+
+    public function compile() {
+
+        $sql = $this->baseQuery;
+        
+        if(!empty($this->conditions)) $sql .= " WHERE $this->conditions";
+        
+        if(!empty($this->orderBy)) $sql .= " ORDER BY $this->orderBy";
+
+        return $sql;
     }
 
 }
 
 
 
+/* #region deprecate */
+
+function getSoqlConditions($values, $fields){
+
+    $conditions = array();
+
+    $fieldsWithValues = array_filter($values, function($value){
+
+        return ($value !== "" && $value !== null);
+    });
+
+    if(empty($fieldsWithValues)) return null;
 
 
+    foreach($fieldsWithValues as $field => $value){
 
+        $syntax = $fields[$field];
 
+        if($syntax == null) continue;
 
+        $value = is_bool($value) ? ($value ? "True" : "False") : $value;
 
-
-
-
-
-
-
-
-
-    function buildCondition($op, $opnd1, $opnd2 = null){
-
-        $sql = "";
-
-        $args = func_get_args();
-
-
-        if($op == "AND" || $op == "OR"){
-
-            array_shift($args);
-
-            return implode(" $op ", array_map(function($item){
-
-                return buildCondition($item["op"], $item["name"], $item["value"]);
-
-            }, $args));
-
-        } else {
-
-            return "$opnd1 $op $opnd2";
-        }
+        $formatted = sprintf($syntax, $value);
+        $conditions[] = $field . " " . $formatted;
     }
+
+    return $conditions;
+}
+
+function buildCondition($op, $opnd1, $opnd2 = null){
+
+    $sql = "";
+
+    $args = func_get_args();
+
+
+    if($op == "AND" || $op == "OR"){
+
+        array_shift($args);
+
+        return implode(" $op ", array_map(function($item){
+
+            return buildCondition($item["op"], $item["name"], $item["value"]);
+
+        }, $args));
+
+    } else {
+
+        return "$opnd1 $op $opnd2";
+    }
+}
+
+/* #endregion */
