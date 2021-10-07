3f85c0a9059e19335c8a4e2f9739462f87f87a51
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-10-06 18:45:53 -0700

    Updated buildConditions.

--- src/SoqlQueryBuilder.php
+++ src/SoqlQueryBuilder.php
@@ -10,6 +10,8 @@ class SoqlQueryBuilder{
 
     public $conditions;
 
+    public $addedConditions;
+
     public $orderBy;
 
 
@@ -21,65 +23,40 @@ class SoqlQueryBuilder{
 
 
 
-    public function buildConditions($conditionGroup){
-
-        // Filter out the conditions where no value is present at the key of "value".  With the exception of grouped conditions.
-        $conditions = array_filter($conditionGroup["conditions"], function($con){
-
-            return $con["isGroup"] == True || ($con["value"] !== null && $con["value"] !== ""); 
-
-        });
+    public function setConditions($conditionGroup){
 
 
-        $sql = "";
-        $joinOperator = $conditionGroup["op"];
+        $this->conditions = $conditionGroup;
+    }
 
-        foreach($conditions as $condition) {
+    public function _buildConditions(){
 
-            if(!is_array($condition)) continue;
-
-            if($condition["isGroup"] != True){      //The condition is not a group of conditions.  It is a single condition.
-
-                $sql .= $this->buildCondition($condition, $joinOperator);
-
-            } else {        //The condition is actually a group of conditions.  Set the join operator to that of the condition subgroup and iterate over the sub conditions.
-                
-                $joinOperator = $condition["op"];
-
-                // Filter out the conditions with no values, and reset the keys...for the loop.
-                $subConditions = array_values(array_filter($condition["conditions"], function($con){
-
-                    return ($con["value"] !== null && $con["value"] !== "");
-                    
-                }));
-
-                for($i = 0; $i < count($subConditions); $i++) {
-
-                    if($i == 0) $sql .= "(";
-
-                    if(!is_array($subconditions[$i])) continue;
-
-                    $sql .= $this->buildCondition($subconditions[$i], $joinOperator);
-                }
-
-                $sql = trim($sql, " $joinOperator");
-                $sql .= ")";
-            }
-        }
-
-        $this->conditions = trim($sql, " $joinOperator");
+        return self::buildConditions($this->conditions);
     }
 
 
-    public function buildCondition($item, $operator){
+        // Build using recursion
+    public static function buildConditions($item) {
 
-        $value = $item["value"];
+        $op = $item["op"];
+        $name = $item["fieldname"];
 
-        $value = is_bool($value) ? ($value ? "True" : "False") : $value;
+    
+        if($op == "AND" || $op == "OR"){
+    
+            return "(" . implode(" $op ", array_map("self::buildConditions", $item["conditions"])) . ")";
+        }
 
-        $formattedValue = sprintf($item["syntax"], $value);
+        if($item["value"] === False || !empty($item["value"])) {
 
-        return $item["fieldname"] . " " . $item["op"] . " $formattedValue $operator ";
+            $value = $item["value"];
+
+            $value = is_bool($value) ? ($value ? "True" : "False") : $value;
+    
+            $formattedValue = sprintf($item["syntax"], $value);
+        }
+    
+        return "$name $op $formattedValue";
     }
 
 
@@ -95,9 +72,44 @@ class SoqlQueryBuilder{
 
 
     // Just takes the entire condition string right now...including the join operator.
-    public function addCondition($entireConditionString) {
+    public function addCondition($conditionString) {
 
-        $this->conditions .= $entireConditionString;
+        $this->addedConditions .= $conditionString;
+    }
+
+    public function mergeValues($fields, $values = null, $removeEmpty = True) {
+
+        if(is_null($values)) return $fields;
+
+        $conditions = $fields["conditions"];
+
+        if($removeEmpty){
+
+            $filtered = array_filter($conditions, function($con) use ($values){
+
+                $key = $con["fieldname"];
+
+                $value = $values[$key];
+
+                return ($value !== "" && $value !== null);
+            });
+            
+        } else {
+
+            $filtered = $conditions;
+        }
+
+        $merged = array_map(function($con) use ($values){
+
+            $key = $con["fieldname"];
+            $value = $values[$key];
+
+            $con["value"] = $value;
+
+            return $con;
+        },$filtered);
+
+        return array("op" => $fields["op"], "conditions" => $merged);
     }
 
 
@@ -105,7 +117,7 @@ class SoqlQueryBuilder{
 
         $sql = $this->baseQuery;
         
-        if(!empty($this->conditions)) $sql .= " WHERE $this->conditions";
+        if(!empty($this->conditions)) $sql .= " WHERE {$this->_buildConditions()} $this->addedConditions";
         
         if(!empty($this->orderBy)) $sql .= " ORDER BY $this->orderBy";
 
@@ -145,27 +157,41 @@ function getSoqlConditions($values, $fields){
     return $conditions;
 }
 
-function buildCondition($op, $opnd1, $opnd2 = null){
 
-    $sql = "";
+    // // Build using recursion
+    // public function buildCondition2($item) {
 
-    $args = func_get_args();
+    //     $sql = "";
 
+    //     $logicalOperator = " AND";
 
-    if($op == "AND" || $op == "OR"){
+    //     foreach($item as $value) {
 
-        array_shift($args);
+    //         if($item["isGroup"] != True){
 
-        return implode(" $op ", array_map(function($item){
+    //             $formattedValue = $this->formatOperandValue($item);
+    //             $sql .= "$logicalOperator {$item['fieldname']} {$item['op']} $formattedValue ";
+                
+    //         } else {
 
-            return buildCondition($item["op"], $item["name"], $item["value"]);
+    //             $logicalOperator = $item["op"];
 
-        }, $args));
+    //             $sql .= "$logicalOperator (";
 
-    } else {
+    //             $subConditions = array_filter($condition["conditions"], function($con){
 
-        return "$opnd1 $op $opnd2";
-    }
-}
+    //                 return ($con["value"] !== null && $con["value"] !== "");
+                    
+    //             });
+
+    //             foreach($subConditions as $condition) $sql .= $this->buildCondition2($condition, $logicalOperator);
+
+    //             $sql .= ") ";
+    //         }
+    //     }
+
+    //     return $sql;
+    // }
+
 
 /* #endregion */
