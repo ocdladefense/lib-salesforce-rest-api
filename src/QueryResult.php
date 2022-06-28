744ccdda4c83ba11ff5f8e34b2a326da3e00a4e7
Jose Bernal <jbernal.web.dev@gmail.com>
2022-06-28 09:56:15 -0700

    Class and methods for working with query results.

---
+++ src/QueryResult.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Salesforce;
+
+
+
+class QueryResult  {
+
+    const DEFAULT_DECODING_SCHEME = "associative_array";
+    const OBJECT_DECODING_SCHEME = "object";
+    const JSON_DECODING_SCHEME = "json";
+
+
+
+    $rows = array();
+
+
+    public function __construct($rows = array()) {
+        $this->rows = $rows;
+    }
+
+
+
+    public function getRecords(){
+
+        return $this->rows;
+    }
+    
+
+    public function getField($fieldName) {
+
+        $records = $this->getRecords();
+		
+		return array_map(function($record) use($fieldName){
+            return $record[$fieldName];
+        }, $records);
+    }
+
+    public function group($fn) {
+
+        $tmp = [];
+
+        foreach($this->getRecords() as $record) {
+            $key = $fn($record);
+            if(!isset($tmp[$key])) {
+                $tmp[$key] = array();
+            }
+
+            $tmp[$key] []= $record;
+        }
+
+        return $tmp;
+    }
+
+    public function getRecord($index = null){
+
+        return $index == null ? $this->body["records"][0] : $this->body["records"][$index];
+    }
+
+    public function getFirst() {
+        $index = 0;
+
+        return $this->body["records"][$index];
+    }
+
+
+    public function getRecordCount(){
+
+        return count($this->getRecords());
+    }
+
+    public function setRecords($records){
+
+        $this->body["records"] = $records;
+    }
+
+
+
+    public function getSObject(){
+        return $SObject;
+    }
+
+    
+
+
+
+    
+}
